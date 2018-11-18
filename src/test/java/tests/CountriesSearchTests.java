ddcdb112de817cddcd3797ab4461f61bfc004c5e
Ha Le <hongha10390@gmail.com>
2018-11-18 11:41:38 +0700

    add src

--- src/test/java/tests/CountriesSearchTests.java
+++ src/test/java/tests/CountriesSearchTests.java
@@ -1,32 +1,34 @@
 package tests;
 
-import io.restassured.RestAssured;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import steps.CountriesSearchSteps;
 
-import static org.hamcrest.Matchers.is;
-
+@RunWith(SerenityRunner.class)
 public class CountriesSearchTests {
+    @Steps
+    CountriesSearchSteps countriesSearchSteps;
+
     @Test
-    public void verifyThatWeCanFindUnitedStatesOfAmericaCountryUsingTheCodeUS(){
-        RestAssured.
-                when().get("http://services.groupkt.com/country/get/iso2code/US").
-                then().assertThat().statusCode(200).
-                and().body("RestResponse.result.name", is("United States of America"));
+    public void verifyThatWeCanFindUnitedStatesOfAmericaCountryUsingTheCodeUS() {
+        countriesSearchSteps.searchCountryByCode("US");
+        countriesSearchSteps.searchIsExecutedSuccesfully();
+        countriesSearchSteps.iShouldFindCountry("United States of America");
     }
 
     @Test
     public void verifyThatWeCanFindIndiaCountryUsingTheCodeIN(){
-        RestAssured.
-                when().get("http://services.groupkt.com/country/get/iso2code/IN").
-                then().assertThat().statusCode(200).
-                and().body("RestResponse.result.name", is("India"));
+        countriesSearchSteps.searchCountryByCode("IN");
+        countriesSearchSteps.searchIsExecutedSuccesfully();
+        countriesSearchSteps.iShouldFindCountry("India");
     }
 
     @Test
     public void verifyThatWeCanFindBrazilCountryUsingTheCodeBR(){
-        RestAssured.
-                when().get("http://services.groupkt.com/country/get/iso2code/BR").
-                then().assertThat().statusCode(200).
-                and().body("RestResponse.result.name", is("Brazil"));
+        countriesSearchSteps.searchCountryByCode("BR");
+        countriesSearchSteps.searchIsExecutedSuccesfully();
+        countriesSearchSteps.iShouldFindCountry("Brazil");
     }
 }
